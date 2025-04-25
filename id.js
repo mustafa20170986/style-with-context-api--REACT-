f37d3b8461292e39ef508b73551f15627cf1a3e6
mustafa20170986 <jster0986@gmail.com>
2025-04-25 11:15:29 -0700

    id token gen and ver

---
+++ id.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken")
+const sec = "i love you emu"
+function gen(user){//genereate id token 
+return jwt.sign({id:user.id},sec,{expiresIn:"10m"})
+}
+function ver(tok){// verify id token 
+    return jwt.verify(tok,sec)
+}
+module.exports ={
+    gen,ver
+}
