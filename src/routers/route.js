f21912a46fd786b180ff9658cb3c47d335b8462e
mustafa20170986 <jster0986@gmail.com>
2025-04-25 11:21:01 -0700

    Delete src directory

--- src/routers/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {createuser,getuser,getusers,deleteuser,updateuser} =require('../controllers/controller')
-const router = express.Router()
-router.post('/',createuser)
-router.get ('/:id',getuser)
-router.get('/',getusers)
-router.delete('/:id',deleteuser)
-router.put('/:id',updateuser)
-module.exports= router
