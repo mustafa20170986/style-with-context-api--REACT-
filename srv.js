b923ac5fc3d3d8bc8a30fdcc549779b4b70ee5fd
mustafa20170986 <jster0986@gmail.com>
2025-04-30 13:14:14 -0700

    server file

--- srv.js
+++ srv.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const routes = require('./r.js')
+const {passport}=require('./mid.js')
 const emu = express()
  const bodyparser =require("body-parser")
  emu.use(bodyparser.json())
  emu.use(routes)
+ emu.use(passport.initialize())
  emu.listen(3000,()=>{
     console.log("emu is listening")
  })
