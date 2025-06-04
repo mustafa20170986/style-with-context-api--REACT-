f3f6080725b7e4dd8e762a138ac4a4d0fcfac775
mustafa20170986 <jster0986@gmail.com>
2025-06-04 00:43:30 -0700

    nginx-project

--- b2.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const emu = express()
-
-emu.get('/',(req,res)=>{
-    return res.status(200).json({msg:"hellow from oni"})
-})
-emu.listen(3001,()=>{
-    console.log('oni is listening')
-})
--- b3.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const emu =express()
-
-emu.get('/',(req,res)=>{
-    return res.status(200).json({msg:"hellow form neha"})
-})
-emu.listen(3002,()=>{
-    console.log("neha is listening")
-})
