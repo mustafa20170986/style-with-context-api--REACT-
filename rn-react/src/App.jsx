19be11414653648589cc4efbe19a5184b932b02a
mustafa20170986 <jster0986@gmail.com>
2025-08-30 06:47:58 -0700

    color picker

---
+++ rn-react/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+//in app.jsx we are just linking the component here 
+import Mn from './components/colorpicker'
+function App(){
+  return (
+   <>
+   <Mn/>
+   </>
+  )
+}
+export default App 
